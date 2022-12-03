e47204d47f663ad48e02748b619e716c58d5c500
OtocolobusManulManul <114613283+OtocolobusManulManul@users.noreply.github.com>
2022-12-02 19:43:47 -0500

    Update dumpCurlopts.py

--- dumpCurlopts.py
+++ dumpCurlopts.py
@@ -1,3 +1,5 @@
+import sys
+
 CURLOPTTYPE_LONG = 0
 CURLOPTTYPE_OBJECTPOINT = 10000
 CURLOPTTYPE_FUNCTIONPOINT = 20000
@@ -6,7 +8,17 @@ CURLOPTTYPE_OFF_T = 30000
 CURLOPTTYPE_STRINGPOINT  = CURLOPTTYPE_OBJECTPOINT
 CURLOPTTYPE_SLISTPOINT  = CURLOPTTYPE_OBJECTPOINT
 
-def CURLOPT(opt, val1, val2): print("#define " + opt, val1 + val2)
+
+modes = ['enum', 'macro']
+output = input("enter output type: (macro or enum): ")
+
+if(output not in modes):
+    print("invalid mode")
+    sys.exit()
+
+def CURLOPT(opt, val1, val2): 
+    if (output == "macro"): print("#define " + opt, val1 + val2)
+    else: print("\t" + opt + " = " + str(val1 + val2) + ",")
 
 with open(input("enter curl.h file path: ")) as curlheader:
     b = curlheader.read()
@@ -22,9 +34,14 @@ while True:
     except ValueError: break
 
 for opt in opts:
-    if(opt == ""): opts.remove("")
+    if(opt == ""): pass
     else:
         optfuncs += [opt.replace(opt[8:opt.index(",")], '"' + opt[8:opt.index(",")] + '"')]
 
+
+if (output == "enum"): print("enum CURLoption\n{")
+
 for opt in optfuncs:
     exec(opt)
+
+print("};")
